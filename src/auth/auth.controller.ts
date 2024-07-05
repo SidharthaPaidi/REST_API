d3b3c864b9a8842839fece7d1e030482f439495a
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-07-05 22:39:27 +0530

    perform e2e test on all models

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ParseIntPipe, Post, Req } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, ParseIntPipe, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { Request } from "express";
 import { AuthDto } from "./dto";
@@ -12,6 +12,7 @@ export class AuthController {
         return this.authservice.signup(dto)
     }
 
+    @HttpCode(HttpStatus.OK)
     @Post('signin')
     signin(@Body() dto : AuthDto) {
         return this.authservice.signin(dto)
